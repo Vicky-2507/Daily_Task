5c45db81d612e0850fc349bb778a57f2c66ecb16
vicky <msvicky904@gmail.com>
2026-02-03 11:00:38 +0530

    tables task update

---
+++ 03-02-2026/tables.js
@@ -0,0 +1,9 @@
+const number = 7; 
+const range = 10; 
+
+console.log(`Multiplication Table :`);
+
+for (let i = 1; i <= range; i++) {
+  const result = i* number;
+console.log(number +"x"+i+"="+(i*number));
+}
