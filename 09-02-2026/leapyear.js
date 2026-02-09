51d412afbd84492b3ba1c7cf2159171ece6bc44a
vicky <msvicky904@gmail.com>
2026-02-09 15:24:52 +0530

    Star Pattern task update

--- 09-02-2026/leapyear.js
+++ 09-02-2026/leapyear.js
@@ -1,13 +1,14 @@
 function LeapYear(year) {
- if( (year % 4 == 0)&&(year % 100 !== 0)||(year % 400 == 0) ){
-     return "True"
+    if ((year % 4 == 0) && (year % 100 !== 0) || (year % 400 == 0)) {
+        return "True"
+    }
+    else {
+        return "False"
+    }
 }
-else {
-    return "False" 
-}
-}
-console.log(LeapYear(2024)); 
-console.log(LeapYear(1900)); 
-console.log(LeapYear(2028)); 
+console.log(LeapYear(2024
+));
+console.log(LeapYear(1900));
+console.log(LeapYear(2028));
 
 
