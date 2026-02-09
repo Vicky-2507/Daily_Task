7bb5d3d46ce36094cd41fc4b6231f3f383835c52
vicky <msvicky904@gmail.com>
2026-02-09 13:44:32 +0530

    Leap Year Task update

---
+++ 09-02-2026/leapyear.js
@@ -0,0 +1,13 @@
+function LeapYear(year) {
+ if( (year % 4 == 0)&&(year % 100 !== 0)||(year % 400 == 0) ){
+     return "True"
+}
+else {
+    return "False" 
+}
+}
+console.log(LeapYear(2024)); 
+console.log(LeapYear(1900)); 
+console.log(LeapYear(2028)); 
+
+
