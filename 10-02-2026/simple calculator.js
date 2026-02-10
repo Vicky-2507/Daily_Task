950f894dfa37089da5037a214c57026a8323a902
vicky <msvicky904@gmail.com>
2026-02-10 14:17:07 +0530

    Simple  Calculator task update

---
+++ 10-02-2026/simple calculator.js
@@ -0,0 +1,15 @@
+function calculator(num1, operator, num2) {
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        default:
+            return "Invalid operator";
+    }
+}
+console.log(calculator(10, '+', 5));
+console.log(calculator(10, '-', 5));
+console.log(calculator(10, '%', 3));  
