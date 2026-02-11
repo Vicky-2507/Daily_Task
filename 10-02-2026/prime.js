d703a06ba16962a700ecba35f036b1af29ad7621
vicky <msvicky904@gmail.com>
2026-02-11 09:54:11 +0530

    prime task update

---
+++ 10-02-2026/prime.js
@@ -0,0 +1,23 @@
+function findPrimes(limit) {
+    const primes = [];
+
+    for (let i = 2; i <= limit; i++) {
+        let isPrime = true;
+
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            primes.push(i);
+        }
+    }
+
+    return primes;
+}
+
+
+console.log(findPrimes(20)); 
